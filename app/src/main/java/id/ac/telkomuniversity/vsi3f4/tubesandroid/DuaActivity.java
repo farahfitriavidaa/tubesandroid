77f3f52654a6f955f495ad7cac95d4c9d2968874
NORISA ERLIANA V <norisa.erlin@gmail.com>
2020-12-07 13:26:36 +0700

    ha hi TampilanLogin Tambahan ayo

---
+++ app/src/main/java/id/ac/telkomuniversity/vsi3f4/tubesandroid/DuaActivity.java
@@ -0,0 +1,11 @@
+package id.ac.telkomuniversity.vsi3f4.tubesandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DuaActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loggin);
+    }
+}
