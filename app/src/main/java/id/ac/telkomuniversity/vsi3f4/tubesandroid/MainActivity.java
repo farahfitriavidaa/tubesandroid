6826e8eff860409883240eb6b2b51207512a4bcf
NORISA ERLIANA V <norisa.erlin@gmail.com>
2020-12-07 13:54:57 +0700

    ha hi TampilanLogin Tambahan ayo bisa

--- app/src/main/java/id/ac/telkomuniversity/vsi3f4/tubesandroid/MainActivity.java
+++ app/src/main/java/id/ac/telkomuniversity/vsi3f4/tubesandroid/MainActivity.java
@@ -2,14 +2,27 @@ package id.ac.telkomuniversity.vsi3f4.tubesandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button pindahButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pindahButton = findViewById(R.id.pindahbutton);
+        pindahButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DuaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     //Haii aku farah
