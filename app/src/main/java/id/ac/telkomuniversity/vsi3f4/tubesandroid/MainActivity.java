48c2cc79cd1887e84efbf43fe64d6de3288e06be
NORISA ERLIANA V <norisa.erlin@gmail.com>
2020-12-13 15:32:26 +0700

    ha hi TampilanLogin Tambahan ayo bisa bisaaa bisaaaaa bisa bulat

--- app/src/main/java/id/ac/telkomuniversity/vsi3f4/tubesandroid/MainActivity.java
+++ app/src/main/java/id/ac/telkomuniversity/vsi3f4/tubesandroid/MainActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button pindahButton;
     Button pindahPlay;
     Button pindahLogin;
+    Button pindahEdit;
+    ImageView gambar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
 
         pindahLogin = findViewById(R.id.button2);
         pindahLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Play.class);
+                startActivity(intent);
+            }
+        });
+
+        pindahEdit = findViewById(R.id.button3);
+        pindahEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Play.class);
