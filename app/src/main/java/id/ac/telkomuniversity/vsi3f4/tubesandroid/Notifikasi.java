54c7cd58afe13d1c23d7ec744de0c60062e0a236
farahfitriavidaa <farahfitriavidaa@gmail.com>
2020-12-17 03:36:15 +0700

    tambah kelas notifikasi dan pembaharuan

--- app/src/main/java/id/ac/telkomuniversity/vsi3f4/tubesandroid/Notifikasi.java
+++ app/src/main/java/id/ac/telkomuniversity/vsi3f4/tubesandroid/Notifikasi.java
@@ -1,23 +1,41 @@
 package id.ac.telkomuniversity.vsi3f4.tubesandroid;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Home extends AppCompatActivity {
+public class Notifikasi extends Activity {
+
+    Button notif;
+    Button pembaruan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.notifikasi);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        notif = findViewById(R.id.button10);
+        pembaruan = findViewById(R.id.button11);
+
+        pembaruan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Notifikasi.this,Pembaruan.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -26,29 +44,28 @@ public class Home extends AppCompatActivity {
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     switch (item.getItemId()) {
                         case R.id.nav_home:
-                            Intent intent = new Intent(Home.this, Home.class);
+                            Intent intent = new Intent(Notifikasi.this, Home.class);
                             startActivity(intent);
                             break;
                         case R.id.nav_favorite:
-                            Intent inten = new Intent(Home.this, Favorit.class);
+                            Intent inten = new Intent(Notifikasi.this, Favorit.class);
                             startActivity(inten);
                             break;
                         case R.id.nav_notificaton:
-                                Intent intens = new Intent(Home.this,Notifikasi.class);
-                                startActivity(intens);
+                            Intent intens = new Intent(Notifikasi.this,Notifikasi.class);
+                            startActivity(intens);
                             break;
                         case R.id.nav_profile:
-                            Intent intern = new Intent(Home.this, Edit.class);
+                            Intent intern = new Intent(Notifikasi.this, Edit.class);
                             startActivity(intern);
                             break;
                         case R.id.nav_search:
-                            Intent inte = new Intent(Home.this, caridua.class);
+                            Intent inte = new Intent(Notifikasi.this, caridua.class);
                             startActivity(inte);
                             break;
                     }
-                    return true;
+                    return false;
                 }
             };
 }
-
 
--- app/src/main/java/id/ac/telkomuniversity/vsi3f4/tubesandroid/Pembaruan.java
+++ app/src/main/java/id/ac/telkomuniversity/vsi3f4/tubesandroid/Pembaruan.java
@@ -1,23 +1,40 @@
 package id.ac.telkomuniversity.vsi3f4.tubesandroid;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Home extends AppCompatActivity {
+public class Pembaruan extends Activity {
+    Button notif;
+    Button pembaruan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.pembaruan);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        notif = findViewById(R.id.button10);
+        pembaruan = findViewById(R.id.button11);
+
+        notif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Pembaruan.this,Notifikasi.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -26,27 +43,27 @@ public class Home extends AppCompatActivity {
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     switch (item.getItemId()) {
                         case R.id.nav_home:
-                            Intent intent = new Intent(Home.this, Home.class);
+                            Intent intent = new Intent(Pembaruan.this, Home.class);
                             startActivity(intent);
                             break;
                         case R.id.nav_favorite:
-                            Intent inten = new Intent(Home.this, Favorit.class);
+                            Intent inten = new Intent(Pembaruan.this, Favorit.class);
                             startActivity(inten);
                             break;
                         case R.id.nav_notificaton:
-                                Intent intens = new Intent(Home.this,Notifikasi.class);
-                                startActivity(intens);
+                            Intent intens = new Intent(Pembaruan.this,Notifikasi.class);
+                            startActivity(intens);
                             break;
                         case R.id.nav_profile:
-                            Intent intern = new Intent(Home.this, Edit.class);
+                            Intent intern = new Intent(Pembaruan.this, Edit.class);
                             startActivity(intern);
                             break;
                         case R.id.nav_search:
-                            Intent inte = new Intent(Home.this, caridua.class);
+                            Intent inte = new Intent(Pembaruan.this, caridua.class);
                             startActivity(inte);
                             break;
                     }
-                    return true;
+                    return false;
                 }
             };
 }
