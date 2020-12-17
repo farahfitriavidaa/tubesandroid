079c6f2705ca12a616fbf131c157f7dabdab715a
farahfitriavidaa <farahfitriavidaa@gmail.com>
2020-12-17 20:22:55 +0700

    perbaharui navbar

--- app/src/main/java/id/ac/telkomuniversity/vsi3f4/tubesandroid/Favorit.java
+++ app/src/main/java/id/ac/telkomuniversity/vsi3f4/tubesandroid/Favorit.java
@@ -1,11 +1,17 @@
 package id.ac.telkomuniversity.vsi3f4.tubesandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Favorit extends Activity {
     private static final String isPlay = "Pemutar Media Favorite";
@@ -57,6 +63,8 @@ public class Favorit extends Activity {
                 playSound(6);
             }
         });
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
     public void onPause(){
         try{
@@ -96,4 +104,34 @@ public class Favorit extends Activity {
         player.setLooping(true);
         player.start();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            Intent intent = new Intent(Favorit.this, Home.class);
+                            startActivity(intent);
+                            break;
+                        case R.id.nav_favorite:
+                            Intent inten = new Intent(Favorit.this, Favorit.class);
+                            startActivity(inten);
+                            break;
+                        case R.id.nav_notificaton:
+                            Intent intens = new Intent(Favorit.this,Notifikasi.class);
+                            startActivity(intens);
+                            break;
+                        case R.id.nav_profile:
+                            Intent intern = new Intent(Favorit.this, Edit.class);
+                            startActivity(intern);
+                            break;
+                        case R.id.nav_search:
+                            Intent inte = new Intent(Favorit.this, caridua.class);
+                            startActivity(inte);
+                            break;
+                    }
+                    return true;
+                }
+            };
 }
